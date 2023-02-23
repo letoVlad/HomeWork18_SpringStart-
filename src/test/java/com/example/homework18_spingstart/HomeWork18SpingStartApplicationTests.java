05bfa30fce892d081ba04ed6a0f3e8054378260b
sonic <evtepo@gmail.com>
2023-02-23 18:26:38 +0300

    HW

---
+++ src/test/java/com/example/homework18_spingstart/HomeWork18SpingStartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homework18_spingstart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork18SpingStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
